ad11a1a21a6652ff586d8d5f95020a3cabbb31e8
Ingo Rohlfing <i.rohlfing@uni-koeln.de>
2020-06-24 21:58:25 +0200

    Polish script

--- Bayes_hypotheses.R
+++ Bayes_hypotheses.R
@@ -47,7 +47,7 @@
 #' move. This is way it has been recommended to break
 #' up $H_0$ and put forward multiple, substantively informative hypotheses.
 #' (In chapter 8 of my case study book and by 
-#' [Fairfield/Charman](https://doi.org/10.1017/pan.2017.14), for example)
+#' [Fairfield/Charman](https://doi.org/10.1017/pan.2017.14), for example.)
 #' 
 #' What happens now when we add a second variable $B$ that is position change
 #' of the *party supporters*? In this case, it makes most no theoretical sense to assume
@@ -64,9 +64,8 @@
 #' let's assume you want to know the probability that a randomly sampled
 #' person is a car owner or a bike owner.  
 #' 
-#' The data is as follows:  
-#' 5 people own a car and a bike; 1 person owns a car; 1 person owns
-#' a bike; 3 people own neither a car nor a bike. 
+#' The data is as follows: 5 people own a car and a bike; 1 person owns a car; 
+#' 1 person owns a bike; 3 people own neither a car nor a bike. 
 #' In a, say, naive analysis, you would once count the number of car owners
 #' (6) and the number of bike owners (6). This gives a sum of 12, meaning
 #' the probability of randomly choosing a car owner or a bike owner is 120%!
@@ -87,10 +86,13 @@ ggVennDiagram(carbike) +
 #' There are two ways to specify the priors when two hypotheses are non-
 #' exclusive. First, you can specify the marginal probability for each hypothesis
 #' and subtract the joint probability of the non-exclusive hypothesis (as discussed by Zaks):
-#' $p(H_A) + p(H_B) - p(H_A \cap H_B)$. If you add $p(H_0) to this, you would
+#' $p(H_A) + p(H_B) - p(H_A \cap H_B)$. If you add $p(H_0)$ to this, you would
 #' get 1 as a result. With regard to the car-bike
-#' example, this means your calculation would be 6+6-5 = 7. A second possibility
-#' is to "exclusivy" each prior (maybe there is a better term for this I don't know.)
+#' example, this means your calculation would be 6+6-5 = 7 and a correct chance of
+#' 70% to randomly choose a car owner or a bike owner. 
+#' 
+#' A second possibility
+#' is to "exclusivy" each prior (maybe there is a better term for this, I don't know.)
 #' The problem with the naive approach is to 
 #' ignore that $p(H_A) = p(H_A \cap H_B) + p(H_A \cap \lnot H_B)$ and that
 #' $p(H_B) = p(H_B \cap H_A) + p(H_B \cap \lnot H_A)$ because of which 
@@ -98,32 +100,40 @@ ggVennDiagram(carbike) +
 #' decompose the total probabilities $p(H_A)$ and $p(H_B)$ into the exclusive
 #' components: 
 #' 
-#' - $p(H_A \cap \lnot H_B)$ (corresponds to the 1 car owner)
-#' - $p(H_B \cap \lnot H_A)$ (corresponds to the 1 bike owner)
-#' - $p(H_A \cap H_B)$ (corresponds to the 5 car and bike owners)
-#' - $p(\lnot H_A \cap \lnot H_B)$ (corresponds to the 3 people without a car
-#' and a bike)
+#' - $p(H_A \cap \lnot H_B)$ (corresponds to the 1 car owner out of 10 people)
+#' - $p(H_B \cap \lnot H_A)$ (corresponds to the 1 bike owner out of 10)
+#' - $p(H_A \cap H_B)$ (corresponds to the 5 car and bike owners out of 10)
+#' - $p(\lnot H_A \cap \lnot H_B)$ (corresponds to the 3 people out of 10
+#' without a car and a bike)
 #' 
 #' I agree with Sheryl Zaks that the specification of priors is more
 #' challenging than one might believe because most examples (methodological and
 #' substantive) use the toy example with two exclusive hypotheses. (And I also
 #' wouldn't accept "Priors do wash out." as a counterargument.) For me, 
 #' exclusifying the priors might be relatively easier because they represent
-#' the different theoretical arguments one can make. $p(H_A \cap \lnot H_B)$ 
-#' means that only hypothesis A is expected to be correct; 
-#' $p(H_B \cap \lnot H_A)$ that only hypothesis B is correct;
-#'  $p(H_A \cap H_B)$ that both hypotheses are correct and both variables 
-#'  (or conditions or mechanisms) play a role. $H_0$ completes the setting
-#'  and is the same as $p(\lnot H_A \cap \lnot H_B)$.
+#' the different theoretical arguments one can make better than the other first
+#' approach. 
+#' 
+#' - $p(H_A \cap \lnot H_B)$ means that only hypothesis A is expected to be correct; 
+#' - $p(H_B \cap \lnot H_A)$ that only hypothesis B is correct;
+#' - $p(H_A \cap H_B)$ that both hypotheses are correct and both variables 
+#'  (or conditions or mechanisms) play a role;
+#'  - $p(\lnot H_A \cap \lnot H_B)$ captures that neither might be correct and
+#'  $H_0$ is true.
 #'  
-#'  Now one might say, I am not interested in the estimation part of 
+#'  Now one might say: I am not interested in the estimation part of 
 #'  a Bayesian analysis (the posteriors for theoretical arguments), but in the
 #'  likelihoods and the testing part where one assesses the strength of evidence 
-#'  (which has been the main focus in the process tracing literature).
-#'  The issue is the same here: When you want to make arguments about the 
+#'  (which has been the main focus in the process tracing literature). I don't need priors
+#'  for this. True, one does not need priors, but the issue is the same here: 
+#'  When you want to make arguments about the 
 #'  evidential strength of an observation for the hypothesis $H_A \cap \lnot H_B$, you need to compare the
-#'  likelihood for the hypothesis $H_A \cap \lnot H_B$ against $H_A \cap H_B$, and separately 
-#'  against $H_B \cap \lnot H_A$ and against $\lnot H_A \cap \lnot H_B$.
+#'  likelihood for $H_A \cap \lnot H_B$ against $H_A \cap H_B$, and separately 
+#'  against $H_B \cap \lnot H_A$ and against $\lnot H_A \cap \lnot H_B$. The hypothesis
+#'  $H_A \cap \lnot H_B$ might receive stronger support in one comparison, but less
+#'  support in a second comparison, which you can only find out when calculating three
+#'  likelihood ratios. (For theoretical reasons, one might reduce the number of comparisons,
+#'  but right now I think it is better to take a more comprehensive perspective.)
 #'  
 #' ### The scale of the issue
 #' The simple car-bike example indicates that the challenges of prior specification
@@ -131,23 +141,24 @@ ggVennDiagram(carbike) +
 #' means that we have to specify four priors instead of two; we have eight priors when
 #' we add a third non-exclusive hypothesis. You might see where this
 #' is going: The number of priors increases
-#' *exponentially* with $2^H$, where $H$ is the number of hypotheses. If some, but not
+#' *exponentially* with $2^H$, where $H$ is the number of substantive hypotheses 
+#' (meaning $H_0$ is excluded). If some, but not
 #' all hypotheses are exclusive, the number is somewhere between the number of
 #' hypotheses, including $H_0$, and $H$.  
 #' Let us add one type of ownership to the example to see how the Venn diagram
 #' gets larger and the number of combinations increases exponentially. We add
-#' a third type 'e-scooter' ownership and are not interested in who either owns
-#' a car, or a bike or an e-scooter. 
+#' a third type 'e-scooter' ownership and are now interested in the probability of
+#' randomly sampling a person who either owns a car, or a bike or an e-scooter. 
 #+ message = F, warning = F
 carbikeesc <- list(car = 1:6, bike = 2:7, escooter = c(2:3, 7:8))
 ggVennDiagram(carbikeesc) +
   theme(legend.position = "none") + 
   scale_fill_gradient(low = "white", high = "gray") 
 
-#' We now have seven types of ownership plus the possibility that someone owns
+#' We now have seven types of ownership constellations plus the possibility that someone owns
 #' neither of the three devices. A naive analysis would again add the total number of
-#' owners per device, which is 16. From the total number of 16, we would need to 
-#' subtract the number of owners that we counted multiple times: the car-bike owners (5),
+#' owners per device, which is 16. A proper analysis would subtract the number of owners that 
+#' we counted multiple times from 16: the car-bike owners (5),
 #' the car-bike-escooter owners (2) and the bike-escooter owners (3). This would 
 #' tell us that 8 people own some device and that the sampling probability is 0.8.
 #'
