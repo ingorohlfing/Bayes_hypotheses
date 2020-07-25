636fc1256f1dfdfded4f53db0a2e7abd971bcbfc
Ingo Rohlfing <i.rohlfing@uni-koeln.de>
2020-07-25 15:26:56 +0200

    Update post

--- Bayes_hypotheses.R
+++ Bayes_hypotheses.R
@@ -3,33 +3,33 @@
 #' author: Ingo Rohlfing
 #' output:
 #'    pdf_document
+#' urlcolor: blue
 #' ---
 
 #' ## Ingo Rohlfing: Multiple non-exclusive hypotheses in Bayesianism 
 #' `r Sys.time()` 
 #' 
-#' In this post, I briefly illustrate for the discrete version of Bayes'
-#' theorem the implications of working with multiple non-exclusive 
-#' hypotheses. I will explain what the specific issue with non-exclusive
-#' hypotheses is, how to take it into account when specifying the theorem
-#' and what the scope of the problem is. 
+#' In this post, I briefly illustrate some implications of multiple 
+#' non-exclusive hypothesesfor the discrete version of Bayes'
+#' theorem. I will explain what the specific issue with non-exclusive
+#' hypotheses is, how to take it into account and what the scope of the problem is. 
 #' 
-#' The post is only a minor extension of what Sheryl Zaks has written about
+#' The post is only a minor extension of what Sherry Zaks has written about
 #' in two articles([2017](https://doi.org/10.1017/pan.2017.12), 
 #' [2020](https://doi.org/10.1017/pan.2020.10)). 
-#' Based on an email exchange I just had with someone
+#' Based on an email exchange I just had with someone else
 #' about this, I thought it might be useful to put up a short post on
-#' this.topic.
+#' this topic.
 #'  
 #' ### The issue with non-exclusive hypotheses
 #' The toy example of Bayes theorem in the *process tracing* literature
 #' uses two exclusive hypotheses and one piece of evidence or a single 
-#' body of evidence $E$. This is totally fine for introducing the theorem
-#' to someone who is not familiar with the theorem (it was for me).
+#' body of evidence $E$. This is fine for introducing the theorem
+#' to someone who is not familiar with it (it was for me).
 #' However, the toy example is as far as away from actual process tracing
-#' because we usually have more than two hypotheses; the hypotheses are not
-#' exclusive, at least not all of them; we have many more pieces of evidence
-#' [Abell (2009) discusses this for more pieces of evidence](https://doi.org/10.1177%2F0049124109339372). 
+#' as possible because we usually have more than two hypotheses; the hypotheses are not
+#' exclusive, at least not all of them; and we have many more pieces of evidence
+#' ([see Abell (2009) on pieces of evidence](https://doi.org/10.1177%2F0049124109339372)). 
 #' 
 #' Concerning the hypotheses, the *priors* for the hypotheses need to add
 #' up to 1. With a substantive hypothesis $H_A$ and a null hypothesis $H_0$, 
@@ -46,8 +46,8 @@
 #' that it is not the median voter, but another cause $B$ that makes parties
 #' move. This is way it has been recommended to break
 #' up $H_0$ and put forward multiple, substantively informative hypotheses.
-#' (In chapter 8 of my case study book and by 
-#' [Fairfield/Charman](https://doi.org/10.1017/pan.2017.14), for example.)
+#' (See [chapter 8 of my case study book](https://www.amazon.com/-/de/Case-Studies-Causal-Inference-Integrative-ebook/dp/B00A208MQ2/ref=sr_1_1?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=rohlfing+case&qid=1595682064&sr=8-1) 
+#' and [Fairfield/Charman](https://doi.org/10.1017/pan.2017.14), for example.)
 #' 
 #' What happens now when we add a second variable $B$ that is position change
 #' of the *party supporters*? In this case, it makes most no theoretical sense to assume
@@ -57,19 +57,21 @@
 #' which means that the null hypothesis $H_0$ is also in the game here.
 #' 
 #' I want to illustrate the implications of two non-exclusive hypotheses 
-#' plus the null hypothesis with a simple example that might be more
-#' intuitive than priors that we attach to hypotheses. Suppose you have
+#' plus the null hypothesis with a simple example. It is not framed in terms of
+#' priors and Bayesian updating (though it could be), but in terms of simple 
+#' probabilities. Suppose you have
 #' data for 10 people and want to know how many own a car ($A$) or a bike
 #' ($B$). To make the example closer to the priors you need for Bayes' theorem,
 #' let's assume you want to know the probability that a randomly sampled
-#' person is a car owner or a bike owner.  
+#' person is a car owner or a bike owner (the equivalent to the prior in this
+#' example).  
 #' 
 #' The data is as follows: 5 people own a car and a bike; 1 person owns a car; 
 #' 1 person owns a bike; 3 people own neither a car nor a bike. 
 #' In a, say, naive analysis, you would once count the number of car owners
 #' (6) and the number of bike owners (6). This gives a sum of 12, meaning
-#' the probability of randomly choosing a car owner or a bike owner is 120%!
-#' The problem obviously is that you count the people
+#' the probability of randomly choosing a car owner or a bike owner is 120%.
+#' The problem obviously is that you count the persons
 #' who own a car and a bike twice. The Venn diagram below illustrates the
 #' issue and also points at ways how it can be addressed. The three people who
 #' neither have a car nor a bike are not explicitly displayed, but can be 
@@ -88,7 +90,7 @@ ggVennDiagram(carbike) +
 #' and subtract the joint probability of the non-exclusive hypothesis (as discussed by Zaks):
 #' $p(H_A) + p(H_B) - p(H_A \cap H_B)$. If you add $p(H_0)$ to this, you would
 #' get 1 as a result. With regard to the car-bike
-#' example, this means your calculation would be 6+6-5 = 7 and a correct chance of
+#' example, your calculation would be 6+6-5 = 7 and a correct chance of
 #' 70% to randomly choose a car owner or a bike owner. 
 #' 
 #' A second possibility
@@ -96,8 +98,8 @@ ggVennDiagram(carbike) +
 #' The problem with the naive approach is to 
 #' ignore that $p(H_A) = p(H_A \cap H_B) + p(H_A \cap \lnot H_B)$ and that
 #' $p(H_B) = p(H_B \cap H_A) + p(H_B \cap \lnot H_A)$ because of which 
-#' $p(H_A \cap H_B)$ is counted twice. To "exclusify" the priors, you
-#' decompose the total probabilities $p(H_A)$ and $p(H_B)$ into the exclusive
+#' $p(H_A \cap H_B)$ is counted twice. To "exclusify" the priors, one can
+#' decompose the marginal probabilities $p(H_A)$ and $p(H_B)$ into the exclusive
 #' components: 
 #' 
 #' - $p(H_A \cap \lnot H_B)$ (corresponds to the 1 car owner out of 10 people)
@@ -106,7 +108,8 @@ ggVennDiagram(carbike) +
 #' - $p(\lnot H_A \cap \lnot H_B)$ (corresponds to the 3 people out of 10
 #' without a car and a bike)
 #' 
-#' I agree with Sheryl Zaks that the specification of priors is more
+#' Using these quantities, one would get the correct total of 7 by adding 1+1+5.
+#' I agree with Sherry Zaks that the specification of priors is more
 #' challenging than one might believe because most examples (methodological and
 #' substantive) use the toy example with two exclusive hypotheses. (And I also
 #' wouldn't accept "Priors do wash out." as a counterargument.) For me, 
@@ -136,7 +139,7 @@ ggVennDiagram(carbike) +
 #'  but right now I think it is better to take a more comprehensive perspective.)
 #'  
 #' ### The scale of the issue
-#' The simple car-bike example indicates that the challenges of prior specification
+#' The simple car-bike example indicates that the challenges of parameter specification
 #' can escalate quickly. Moving from the toy example to two non-exclusive hypotheses
 #' means that we have to specify four priors instead of two; we have eight priors when
 #' we add a third non-exclusive hypothesis. You might see where this
@@ -144,7 +147,7 @@ ggVennDiagram(carbike) +
 #' *exponentially* with $2^H$, where $H$ is the number of substantive hypotheses 
 #' (meaning $H_0$ is excluded). If some, but not
 #' all hypotheses are exclusive, the number is somewhere between the number of
-#' hypotheses, including $H_0$, and $H$.  
+#' hypotheses, including $H_0$, and $2^H$.  
 #' Let us add one type of ownership to the example to see how the Venn diagram
 #' gets larger and the number of combinations increases exponentially. We add
 #' a third type 'e-scooter' ownership and are now interested in the probability of
@@ -167,3 +170,8 @@ ggVennDiagram(carbikeesc) +
 #' what it requires to do process tracing that complies with Bayes' theorem.
 #' It quickly becomes demanding when we work with non-exclusive hypotheses
 #' that are, in my view, the rule in the social sciences.
+#' 
+#' ### Packages
+#' Chun-Hui Gao (2019). *ggVennDiagram: A 'ggplot2' Implement of Venn Diagram*.
+#' R package version 0.3. <https://CRAN.R-project.org/package=ggVennDiagram>  
+#' H. Wickham. *ggplot2: Elegant Graphics for Data Analysis*. Springer-Verlag New York, 2016.
